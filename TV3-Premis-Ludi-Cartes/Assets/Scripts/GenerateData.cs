206c10f045d4be17762bf26dd7623af4c4369544
Sara-MM <sqmartinezm@gmail.com>
2024-10-17 00:08:53 +0200

    arreglos de csv y code

--- TV3-Premis-Ludi-Cartes/Assets/Scripts/GenerateData.cs
+++ TV3-Premis-Ludi-Cartes/Assets/Scripts/GenerateData.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class GenerateData : MonoBehaviour
 {
-    public List<WordsCombinationCheck> wordsCheckerDataList;
-    public List<WordData> wordsDataList;
+    public List<Word> wordsList = new List<Word>();
 
     // Start is called before the first frame update
     void Start()
     {
-        wordsCheckerDataList = ReadCSV.Read<WordsCombinationCheck>("CSV/WordsCheck");
+        List<WordsCombinationCheck> wordsCheckerDataList = ReadCSV.Read<WordsCombinationCheck>("CSV/WordsCheck");
 
         foreach (WordsCombinationCheck data in wordsCheckerDataList)
         {
@@ -18,11 +17,14 @@ public class GenerateData : MonoBehaviour
                 data.wSubstantiu, data.wAdjectiu, data.wVerb, data.wAdverbi, data.wArticle, data.wPronom);
         }
 
-        wordsDataList = ReadCSV.Read<WordData>("CSV/Props");
+        List<WordData> wordsDataList = ReadCSV.Read<WordData>("CSV/WordsList");
 
         foreach (WordData data in wordsDataList)
         {
-            data.CreateWord(data.word, data.type);
+            Globals.wordsList.Add(data.CreateWord(data.word, data.type));
         }
+
+        // DEBUG
+        wordsList = Globals.wordsList;
     }
 }
--- TV3-Premis-Ludi-Cartes/Assets/Scripts/Globals.cs
+++ TV3-Premis-Ludi-Cartes/Assets/Scripts/Globals.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class Globals
 {
-    //static public Dictionary<WORD_TYPES, bool> wordsCheckDictionarySubstantiu = new Dictionary<WORD_TYPES, bool>();
-    //static public Dictionary<WORD_TYPES, bool> wordsCheckDictionaryAdjectiu = new Dictionary<WORD_TYPES, bool>();
-    //static public Dictionary<WORD_TYPES, bool> wordsCheckDictionaryVerb  = new Dictionary<WORD_TYPES, bool>();
-    //static public Dictionary<WORD_TYPES, bool> wordsCheckDictionaryAdverbi = new Dictionary<WORD_TYPES, bool>();
-    //static public Dictionary<WORD_TYPES, bool> wordsCheckDictionaryArticle = new Dictionary<WORD_TYPES, bool>();
-    //static public Dictionary<WORD_TYPES, bool> wordsCheckDictionaryPronom = new Dictionary<WORD_TYPES, bool>();
-     
     static public Dictionary<WORD_TYPES, Dictionary<WORD_TYPES, bool>> wordsCheckDictionary =
         new Dictionary<WORD_TYPES, Dictionary<WORD_TYPES, bool>>();
 
@@ -18,18 +11,16 @@ public class Globals
 
     static public void CreateWordsCombinationCheck(WORD_TYPES type, bool wSubstantiu, bool wAdjectiu, bool wVerb, bool wAdverbi, bool wArticle, bool wPronom)
     {
-        wordsCheckDictionary.Add(type, new Dictionary<WORD_TYPES, bool>() { { WORD_TYPES.SUBSTANTIU, wSubstantiu } });
-        wordsCheckDictionary.Add(type, new Dictionary<WORD_TYPES, bool>() { { WORD_TYPES.ADJECTIU, wAdjectiu } });
-        wordsCheckDictionary.Add(type, new Dictionary<WORD_TYPES, bool>() { { WORD_TYPES.VERB, wVerb } });
-        wordsCheckDictionary.Add(type, new Dictionary<WORD_TYPES, bool>() { { WORD_TYPES.ADVERBI, wAdverbi } });
-        wordsCheckDictionary.Add(type, new Dictionary<WORD_TYPES, bool>() { { WORD_TYPES.ARTICLE, wArticle } });
-        wordsCheckDictionary.Add(type, new Dictionary<WORD_TYPES, bool>() { { WORD_TYPES.PRONOM, wPronom } });
+        Dictionary<WORD_TYPES, bool> temp = new Dictionary<WORD_TYPES, bool>
+        {
+            { WORD_TYPES.SUBSTANTIU, wSubstantiu },
+            { WORD_TYPES.ADJECTIU, wAdjectiu },
+            { WORD_TYPES.VERB, wVerb } ,
+            { WORD_TYPES.ADVERBI, wAdverbi },
+            { WORD_TYPES.ARTICLE, wArticle},
+            { WORD_TYPES.PRONOM, wPronom }
+        };
 
-        //Globals.wordsCheckDictionarySubstantiu.Add(WORD_TYPES.SUSTANTIU, wSubstantiu);
-        //Globals.wordsCheckDictionaryAdjectiu.Add(WORD_TYPES.ADJECTIU, wAdjectiu);
-        //Globals.wordsCheckDictionaryVerb.Add(WORD_TYPES.VERB, wVerb);
-        //Globals.wordsCheckDictionaryAdverbi.Add(WORD_TYPES.ADVERBI, wAdverbi);
-        //Globals.wordsCheckDictionaryArticle.Add(WORD_TYPES.ARTICLE, wArticle);
-        //Globals.wordsCheckDictionaryPronom.Add(WORD_TYPES.PRONOM, wPronom);
+        wordsCheckDictionary.Add(type, temp);
     }
 }
