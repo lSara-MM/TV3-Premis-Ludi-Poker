d292f71778466a3119237bee43ccb6ff753406a9
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-10-24 16:51:53 +0200

    Calculate score to beat

--- TV3-Premis-Ludi-Cartes/Assets/Scripts/GameManager.cs
+++ TV3-Premis-Ludi-Cartes/Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private int goalScore;
     [SerializeField] private int playerScore;
+    public int currentLevel = 1;
 
     [SerializeField] private PlayCards cs_PlayCards;
 
@@ -33,5 +34,15 @@ public class GameManager : MonoBehaviour
         //{
 
         //}
+    }
+
+    int SetGoalScore(int level) 
+    {
+        int baseScore = 300;
+        int linearScaling = level * 45;
+        int escalingDificulty = (level / 5) * 150;
+        int exponentialDificulty = (level / 3) * (level / 3 + 1) / 2 * 45;
+
+        return baseScore+linearScaling+escalingDificulty+exponentialDificulty;
     }
 }
