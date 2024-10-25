b9e3d37504f9333e475aabc5f94c0dafc3e92a80
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-10-25 12:10:20 +0200

    WIP upgrade

--- TV3-Premis-Ludi-Cartes/Assets/Scripts/DeckUpgradeThings/RandomUpgradesManager.cs
+++ TV3-Premis-Ludi-Cartes/Assets/Scripts/DeckUpgradeThings/RandomUpgradesManager.cs
@@ -11,14 +11,46 @@ public class UpgradesManager : MonoBehaviour
     [SerializeField] List<int> upgradeOptions; //Esto es un WIP, la idea seria cojer de una lista de structs con un ENUM para la funcion y rareza
     int op1 = -1, op2 = -1; //To not repeat type of upgrades
 
+    public enum UPGRADES_RARITIES 
+    {
+        COMMON,
+        RARE,
+        LEGENDARY
+    }
+    public UPGRADES_RARITIES ownRarity; //Esto es temporal, tendra que ser asignarle a cada 
+
     void Start()
     {
         foreach(GameObject go in UIobjects) 
         {
-            //Here would be a switch based on rarity
-            go.GetComponent<Image>().color = Color.white;
+            int rarity = Random.Range(1, 20); 
+            if (rarity < 12) //Aqui habria que setearlo en el go no lo de ownRarity
+            {
+                ownRarity = UPGRADES_RARITIES.COMMON; 
+                
+            }
+            else if (rarity > 19)
+            {
+                ownRarity = UPGRADES_RARITIES.LEGENDARY;
+            }
+            else 
+            {
+                ownRarity = UPGRADES_RARITIES.RARE;
+            }
 
-            Random.Range(0, 10); //10 would be the enum size
+            //Here would be a switch based on rarity
+            switch (ownRarity)
+            {
+                case UPGRADES_RARITIES.COMMON:
+                    go.GetComponent<Image>().color = Color.green; //Green
+                    break;
+                case UPGRADES_RARITIES.RARE:
+                    go.GetComponent<Image>().color = Color.cyan; //Cyan
+                    break;
+                case UPGRADES_RARITIES.LEGENDARY:
+                    go.GetComponent<Image>().color = new Color(1,0,1); //Purple
+                    break;
+            };
         }
     }
 
