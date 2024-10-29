64754252d2706a28d26bfd0e42a7e796cc1f26d3
AndyCubico <astartes99@gmail.com>
2024-10-29 23:20:14 +0100

    buttons combo book

--- TV3-Premis-Ludi-Cartes/Assets/Scripts/Hide GameObject.cs
+++ TV3-Premis-Ludi-Cartes/Assets/Scripts/Hide GameObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowAndHideGameObject : MonoBehaviour
 {
@@ -10,23 +11,27 @@ public class ShowAndHideGameObject : MonoBehaviour
 
     [SerializeField] GameObject goActive;
     [SerializeField] GameObject goInactive;
+
+    // Forgive me
+    [SerializeField] GameObject buttonActive;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void HideGO(int mode = 0) //Default activa y desactiva
     {
-        
+
         foreach (GameObject go in gameObjects)
         {
-            go.SetActive( (!go.activeInHierarchy || (mode>0)) && !(mode<0)); //Si mode <0 entonces solo puede desactivar porque siempre sera falso en el and, si mode>0 siempre sera cierto porque true en el or
+            go.SetActive((!go.activeInHierarchy || (mode > 0)) && !(mode < 0)); //Si mode <0 entonces solo puede desactivar porque siempre sera falso en el and, si mode>0 siempre sera cierto porque true en el or
         }
 
     }
@@ -40,5 +45,9 @@ public class ShowAndHideGameObject : MonoBehaviour
     {
         goActive.SetActive(true);
         goInactive.SetActive(false);
+
+        gameObject.GetComponent<Button>().interactable = false;
+
+        buttonActive.GetComponent<Button>().interactable = true;
     }
 }
