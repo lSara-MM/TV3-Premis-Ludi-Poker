e2526a80a9427d9283f139fe1c4d3de75b505eb7
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-10-29 20:27:57 +0100

    Creditos moverse
    Co-Authored-By: Andreu Nosàs Soler <99950309+AndyCubico@users.noreply.github.com>

--- TV3-Premis-Ludi-Cartes/Assets/Scripts/MoveCredits.cs
+++ TV3-Premis-Ludi-Cartes/Assets/Scripts/MoveCredits.cs
@@ -8,6 +8,9 @@ public class MoveCredits : MonoBehaviour
     [SerializeField] Camera screenCamera;
     float dt = 0;
     public float waitMove = 0.1f;
+    public int jumps = 20;
+
+    float endOfCanvas; //This asumes that is on the border
     // Start is called before the first frame update
     void Start()
     {
@@ -15,18 +18,19 @@ public class MoveCredits : MonoBehaviour
         {
             picture = this.gameObject.GetComponent<RectTransform>();
         }
+        endOfCanvas = picture.rect.yMin;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (picture.anchoredPosition.y < 2000)
+        if (picture.anchoredPosition.y < -endOfCanvas*2.9) //Con tres le asoma la puntita
         {
             dt += Time.deltaTime;
 
             if (dt > waitMove)
             {
-                picture.anchoredPosition = new Vector2(picture.anchoredPosition.x, picture.anchoredPosition.y + 20);
+                picture.anchoredPosition = new Vector2(picture.anchoredPosition.x, picture.anchoredPosition.y + jumps);
                 dt = 0;
             }
         }
